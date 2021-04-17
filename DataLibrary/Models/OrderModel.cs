6df90d99cd663cbee868afaefaea017e5bf67fb7
Jeremiah Ogunbor <jerryogunbor@gmail.com>
2021-04-17 16:13:28 +0100

    Add project files.

---
+++ DataLibrary/Models/OrderModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    public class OrderModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25, ErrorMessage = "You need to keep the name to a mex of 25 characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters")]
+        [DisplayName("Name for the order")]
+        public string OrderName { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        [DisplayName("Meal")]
+        [Range(1, int.MaxValue, ErrorMessage ="You need to select a meal from the list")]
+        public int FoodId { get; set; }
+        [Required]
+        [Range(1, 10, ErrorMessage ="You can select up to 10 meals")]
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ JerrysDinnerDB/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,20 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+if not exists (select * from dbo.Food)
+begin
+  insert into dbo.Food(Title, [Description], Price)
+  values ('Fried rice', 'Fried rice and a drink', 600), 
+         ('Meatpie', 'Meatpie and a drink', 300), 
+         ('Salad', 'A large salad', 400);
+end
